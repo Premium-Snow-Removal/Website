a8dadffda826d3836f501534c7f63904abd02389
SnowRemovalCoder1 <34726646+SnowRemovalCoder1@users.noreply.github.com>
2017-12-22 19:08:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function main() {
   var active = false;
+  /*Above variable is wether or not PSR is in service*/
   var active2 = "are not";
   var message = "Sorry :(";
   if (active == true) {
     active2 = "are";
-    message = ""
+    message = "";
   }
 document.getElementById("snowCounter").innerHTML = "We " + active2 + " in service at this time. " + message;
 }
-setTimeout(main, 500)
+setTimeout(main, 500);
