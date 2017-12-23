a985ab5c9d23eb1538570c2ef7287c6566da7ba0
SnowRemovalCoder1 <34726646+SnowRemovalCoder1@users.noreply.github.com>
2017-12-23 09:01:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
- var active = false;
- /*Above variable is wether or not PSR is in service*/
+var minuteToSecond = 60;
+var SecondToMillisecond = 1000;
+var updateTimeMinutes = 30;
+var updateTimeFinal = updateTimeMinutes * minuteToSecond * SecondToMillisecond;
+
+function timedRefresh() {
+	setTimeout("location.reload(true);", updateTimeFinal);
+ //1800000 is equal to 30m, which is the time frame for updates on stats like report
+}
+
+window.onload = timedRefresh();
+//Above is code used for refreshing the page every 30m
+
+var active = false;
+ //Above variable is wether or not PSR is in service
 function main() {
   var active2 = "are not";
   var message = "Sorry :(";
   if (active == true) {
     active2 = "are";
-    message = "";
+    message = ":)";
   }
 document.getElementById("snowCounter").innerHTML = "We " + active2 + " in service at this time. " + message;
 }
