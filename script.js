881a9305317fec7354813350c8c71ab9c688a0d8
SnowRemovalCoder1 <34726646+SnowRemovalCoder1@users.noreply.github.com>
2017-12-22 18:58:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function main() {
-var active = false;
-getElementByID
+  var active = false;
+  var active2 = "are not";
+  var message = "Sorry :(";
+  if (active = true) {
+    active2 = "are";
+    message = ""
+  }
+document.getElementById("snowCounter").innerHTML = "We " + active2 + " in service at this time. " + message;
 }
 setTimeout(main, 500)
